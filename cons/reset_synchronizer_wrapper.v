b04127996f91b2168211636af7ee1b24c2133c64
karthikeyaneti <karthikneti17@gmail.com>
2026-09-10 05:09:58 +0000

    synchronizer wrappers and sgdc files

--- cons/reset_synchronizer_wrapper.v
+++ cons/reset_synchronizer_wrapper.v
@@ -6,10 +6,19 @@ module reset_synchronizer_wrapper (
     output wire rst_n_sync
 );
 
+    reg reset_probe;
+
     reset_synchronizer u_sync (
         .clk        (clk),
         .rst_n_async(rst_n_async),
         .rst_n_sync (rst_n_sync)
     );
+
+    always @(posedge clk or negedge rst_n_sync) begin
+        if (!rst_n_sync)
+            reset_probe <= 1'b0;
+        else
+            reset_probe <= 1'b1;
+    end
 
 endmodule
